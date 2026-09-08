3a9011731118454eed28fc98d9b8365476233a76
Andres30211 <felifit27@gmail.com>
2026-09-08 09:52:21 -0500

    Mejoras

--- src/main/java/neumatica/location/service_location_neumatica/configuration/SecurityConfig.java
+++ src/main/java/neumatica/location/service_location_neumatica/configuration/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
                          */
                         .requestMatchers(
                                 "/api/visits/**",
-                                "/uploads/**"
+                                "/api/visits/uploads/**"
                         ).permitAll()
 
 
